2fc1292c58be0743127002cdf394ba22eff84114
brotherilia <ilyamenshikov@gmail.com>
2016-04-28 01:07:57 +0300

    Структурировал исходники. Наладил сборку и деплой.

---
+++ Gruntfile.js
@@ -0,0 +1,181 @@
+"use strict";
+
+module.exports = function(grunt) {
+  require("load-grunt-tasks")(grunt);
+
+  require("time-grunt")(grunt);
+
+  grunt.initConfig({
+
+    //*** Очистка ***//
+    clean: {
+      build: ["build"]
+    },
+
+    //*** Копирование ***//
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "src/",
+          src: [
+            "fonts/**/*.{eot,ttf,woff,woff2}",
+            "img/*.{jpg,png,gif,svg}",
+            "js/**/*.js",
+            "*.html"
+          ],
+          dest: "build"
+        }]
+      },
+      html: {
+        files: [{
+          expand: true,
+          cwd: "src/",
+          src: ["*.html"],
+          dest: "build"
+        }]
+      },
+      js: {
+        files: [{
+          expand: true,
+          cwd: "src/",
+          src: ["js/**/*.js"],
+          dest: "build"
+        }]
+      }
+    },
+
+    //*** Сборка CSS из LESS ***//
+    less: {
+      style: {
+        files: {
+          "build/css/style.css": "src/less/style.less"
+        }
+      }
+    },
+
+    //*** Обработка CSS: префиксование ***//
+    postcss: {
+      options: {
+        processors: [
+          require("autoprefixer")({browsers: [
+            "last 2 versions",
+            "> 1%"
+          ]})
+        ]
+      },
+      style: {
+        src: "build/css/*.css"
+      }
+    },
+
+    //*** "Причесывание" CSS ***//
+    csscomb: {
+      style: {
+        options: {
+          config: "csscomb.json"
+        },
+        files: {
+          "build/css/style.css": ["build/css/style.css"]
+        }
+      }
+    },
+
+    //*** Минификация CSS ***//
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
+        }
+      }
+    },
+
+    //*** Минификация графики ***//
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png,jpg,gif}"]
+        }]
+      }
+    },
+
+    //*** Минификация JS ***//
+    uglify: {
+      options: {
+        mangle: false
+      },
+      scripts: {
+        files: {
+          "build/js/index.min.js": ["build/js/index.js"],
+          "build/js/catalog.min.js": ["build/js/catalog.js"]
+        }
+      }
+    },
+
+    //*** Локальный сервер с обновлением браузера ***//
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "build/*.html",
+            "build/css/*.css",
+            "build/js/*.js"
+          ]
+        },
+        options: {
+          server: "build/",
+          watchTask: true,
+          notify: false,
+          open: true,
+          ui: false
+        }
+      }
+    },
+
+    //*** Отслеживание изменений в исходниках ***//
+    watch: {
+      html: {
+        files: ["src/*.html"],
+        tasks: ["copy:html"],
+        options: {spawn: false}
+      },
+      style: {
+        files: ["src/less/**/*.less"],
+        tasks: ["less", "postcss", "csscomb", "csso"],
+        options: {spawn: false}
+      },
+      scripts: {
+        files: ["src/js/**/*.js"],
+        tasks: ["copy:js","uglify"],
+        options: {spawn: false}
+      }
+    },
+
+    //*** Отправка сборки в удаленную ветку "gh-pages" ***//
+    "gh-pages": {
+      options: {
+        base: "build"
+      },
+      src: "**/*"
+    }
+
+  });
+
+  grunt.registerTask("serve", ["browserSync", "watch"]);
+  grunt.registerTask("css", ["less", "postcss", "csscomb", "csso"]);
+  grunt.registerTask("build", [
+    "clean",
+    "copy:build",
+    "css",
+    "imagemin",
+    "uglify"
+  ]);
+
+};
